0178fcf5cffc8d2474670f41fad9a7b4f7288460
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2017-12-05 17:09:51 +0300

    Testing

--- ClassLibraryOfMemes/Repository.cs
+++ ClassLibraryOfMemes/Repository.cs
@@ -108,14 +108,14 @@ namespace ClassLibraryOfMemes
                 throw new Exception("No delete was provided succesfully.");
             }
         }
-        //public byte[] SetBitmat(Bitmap bmp)// method was taken from https://ru.stackoverflow.com/questions/72679/%D0%9A%D0%B0%D0%BA-%D0%B7%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D1%82%D1%8C-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D1%83-%D0%B2-%D0%B1%D0%B0%D0%B7%D1%83-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D0%B8-%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%B8%D1%82%D1%8C-%D1%84%D0%BE%D1%82%D0%BE-%D0%B2-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B5-picture
-        //{
+        public byte[] SetBitmat(Bitmap bmp)// method was taken from https://ru.stackoverflow.com/questions/72679/%D0%9A%D0%B0%D0%BA-%D0%B7%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D1%82%D1%8C-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D1%83-%D0%B2-%D0%B1%D0%B0%D0%B7%D1%83-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D0%B8-%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%B8%D1%82%D1%8C-%D1%84%D0%BE%D1%82%D0%BE-%D0%B2-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B5-picture
+        {
 
-        //    using (MemoryStream ms = new MemoryStream())
-        //    {
-        //        bmp.Save(ms, bmp.RawFormat);
-        //        return ms.ToArray();
-        //    }
-        //}
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bmp.Save(ms, bmp.RawFormat);
+                return ms.ToArray();
+            }
+        }
     }
     }
